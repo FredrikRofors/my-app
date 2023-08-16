c171b4a67d3a3f454b8b3908d8e58511d0f608e3
Fredrik Rofors <fredrik_rofors@hotmail.com>
2023-08-16 14:06:36 +0200

    And more linting...

--- app/components-client/myheader.tsx
+++ app/components-client/myheader.tsx
@@ -4,7 +4,9 @@ interface Props {
     headerText: string;
 }
 
-export default function MyHeader({ headerText } : Props) {
+export default function MyHeader(props: Props) {
+    const { headerText } = props;
+
     return (
         <>
             <h1>{headerText}</h1>
